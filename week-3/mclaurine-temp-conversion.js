95a54e4659af8781715094d618b96137fe56fab9
Trevor McLaurine <trevor.mclaurine@gmail.com>
2023-01-16 13:16:48 -0600

    Added comments

--- week-3/mclaurine-temp-conversion.js
+++ week-3/mclaurine-temp-conversion.js
@@ -1,9 +1,20 @@
+/*
+    ==================
+    Title: mclaurine-temp-conversion.js, 
+    Author: Trevor McLaurine
+    Date: 1/16/2023
+    Description: Demonstrates how to create an app to convert fahrenheit to celsius.
+*/
+
+// This bit of code checks to see if the HTML document has loaded.
 window.addEventListener("DOMContentLoaded", () => { 
 
+    // Finds and links various html elements to variables.
     const convertButton = document.getElementById('convertTempButton')
     const temperatureResults = document.getElementById('results')
     const userInput = document.getElementById('txtFahrenheit')
 
+    //This is where the temperature is converted utilizing the user input to create the resulting temperature.
     function convertTemperature(e) {
         e.preventDefault(); 
         let fahrenheit = userInput.value;
@@ -11,7 +22,9 @@ window.addEventListener("DOMContentLoaded", () => {
         return parseFloat((fahrenheit - 32) / 1.8).toFixed(2)
     }
 
+    //This adds the event listener so when the user clicks the button it performs the conversion function
     convertButton.addEventListener('click', (e) => {
+        //This sets the value of the temperature results div to the value received from the convertTemperature function.
         temperatureResults.innerHTML = convertTemperature(e)
     })
 
